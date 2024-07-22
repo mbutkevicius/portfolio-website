64c974572336f3b6e4a3ca467ded986107f2f820
Michael Butkevicius <mjbutkevicius@gmail.com>
2024-07-21 21:18:01 -0500

    Added Nav bar for desktop and mobile

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Introduction from "./components/Introduction";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <div class="container mx-auto px-12 py-4">
+      <Navbar />
+      <div class="container mt-28 mx-auto px-12 py-4">
         <Introduction />
       </div>
     </main>
