45cc98e476b7b3a379382ad7c76756e224fab1fa
Michael Butkevicius <mjbutkevicius@gmail.com>
2024-07-25 17:19:05 -0500

    Updated meta data for displaying in browser

--- src/app/page.js
+++ src/app/page.js
@@ -10,22 +10,31 @@ import AchievmentsSection from './components/AchievementsSection'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
-      <Navbar />
-      <div className="container mt-28 mx-auto px-12 py-4">
-        <IntroductionSection />
-        <AchievmentsSection />
-        <section id="about">
-          <AboutSection />
-        </section>
-        <section id="projects">
-          <ProjectsSection />
-        </section>
-        <section id="socials">
-          <EmailSection />
-        </section>
-      </div>
-      <Footer />
-    </main>
+    <>
+      <Head>
+        <title>Michael Butkevicius&apos; Portfolio</title>
+        <meta name="description" content="A portfolio made in React to showcase projects I have worked on." />
+        <meta property="og:title" content="Michael Butkevicius&apos; Portfolio" />
+        <meta property="og:description" content="A portfolio made in React to showcase projects I have worked on." />
+        <meta property="og:url" content="https://portfolio-website-zeta-bice.vercel.app/" />
+      </Head>
+      <main className="flex min-h-screen flex-col bg-[#121212]">
+        <Navbar />
+        <div className="container mt-28 mx-auto px-12 py-4">
+          <IntroductionSection />
+          <AchievmentsSection />
+          <section id="about">
+            <AboutSection />
+          </section>
+          <section id="projects">
+            <ProjectsSection />
+          </section>
+          <section id="socials">
+            <EmailSection />
+          </section>
+        </div>
+        <Footer />
+      </main>
+    </>
   );
 }
