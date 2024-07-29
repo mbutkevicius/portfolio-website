9598003c77215caab56bfb06697ad063523b7193
Michael Butkevicius <mjbutkevicius@gmail.com>
2024-07-28 22:35:05 -0500

    Updated button text color

--- src/app/components/IntroductionSection.jsx
+++ src/app/components/IntroductionSection.jsx
@@ -43,7 +43,7 @@ const IntroductionSection = () => {
                     <a 
                         href='/resume/ButkeviciusResume.pdf'
                         download='ButkeviciusResume.pdf'
-                        className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-green-200 via-green-500 to-green-800 hover:from-green-400 hover:via-green-700 hover:to-green-800 hover:bg-red-500 text-black'
+                        className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-green-200 via-green-500 to-green-800 hover:from-green-400 hover:via-green-700 hover:to-green-800 hover:bg-red-500 text-white'
                     >
                     Download CV
                     </a>
